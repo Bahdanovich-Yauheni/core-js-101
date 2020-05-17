0b6d574addfcb524195aa24522e34c952577bcd9
Yauheni Bahdanovich <bahdanovich.yauheni@gmail.com>
2020-05-17 23:54:15 +0300

    add: last changes

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -298,8 +298,20 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  let sum = 0;
+  for (let i = 0; i < ccn.length; i += 1) {
+    let cardNum = parseInt(ccn[i], 10);
+    if ((ccn.length - i) % 2 === 0) {
+      cardNum *= 2;
+      if (cardNum > 9) {
+        cardNum -= 9;
+      }
+    }
+    sum += cardNum;
+  }
+  // console.log(sum);
+  return sum % 10 === 0;
 }
 
 /**
